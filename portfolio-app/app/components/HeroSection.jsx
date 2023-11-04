dfe750f9f2f79973baaf5480195b189446270dbb
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-11-04 01:04:27 -0700

    update

---
+++ portfolio-app/app/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const HeroSection = () => {
+    return (
+        <section>
+                <h1 className="text-white mb-4 text-6xl font-extrabold">
+                    Whats up its Tray
+                </h1>
+                <p className="text-[#adb7be]">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+        </section>
+    );
+};
+
+export default HeroSection;
