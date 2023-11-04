254230d199ff793aa318a766d78129b563915cfc
ilike3p <39431924+ilike3p@users.noreply.github.com>
2023-11-04 02:01:52 -0700

    update

--- portfolio-app/app/components/HeroSection.jsx
+++ portfolio-app/app/components/HeroSection.jsx
@@ -3,12 +3,16 @@ import React from 'react';
 const HeroSection = () => {
     return (
         <section>
-                <h1 className="text-white mb-4 text-6xl font-extrabold">
+            <div className="grid grid-cols-1 lg:grid-cols-12">
+                <div className='col-span-7'>
+                <h1 className="text-white mb-4 text-4xl lg:text-6xl font-extrabold">
                     Whats up its Tray
                 </h1>
-                <p className="text-[#adb7be]">
+                <p className="text-[#adb7be] text-lg lg:text-xl">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 </p>
+                </div>
+            </div>
         </section>
     );
 };
--- portfolio-app/app/page.js
+++ portfolio-app/app/page.js
@@ -1,9 +1,9 @@
 import Image from 'next/image'
-import HeroSection from ""./components/HeroSection";
+import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto">
+    <main className="flex min-h-screen flex-col bg-[#121212] container mx-auto px-12">
       <HeroSection />
     </main>
   );
